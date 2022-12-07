09f27a139d9cc027002e261539c690c3b19bb767
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-07 16:55:48 +0000

    update the webpack set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
+  element.classList.add('hello');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
   },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/home.html',
+    }),
+  ]
 };
